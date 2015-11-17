681de6d132bb48b4893890888d97dd0f65ec4e88
xzereha <xzereha@users.noreply.github.com>
2015-11-17 23:22:32 +0100

    Bytte till svenska(typ)

--- RaceTurtle.java
+++ RaceTurtle.java
@@ -9,8 +9,8 @@ public class RaceTurtle extends Turtle{
 	private Random rand = new Random();
 	private final int MAX_STEP_LENGTH = 6;
 	/**
-	 * Creates a RaceTurtle in the window w with the starting number nbr.
-	 * The turtle will get appropriate starting position depending on the start number.
+	 * Skapar en RaceTurtle i RaceWindow "w" med start nummer "nbr".
+	 * RaceTurtlen får passande startposition beroende på start nummert.
 	 */
 	public RaceTurtle(RaceWindow w, int nbr){
 		super(w, RaceWindow.getStartXPos(nbr), RaceWindow.getStartYPos(nbr));
@@ -20,12 +20,16 @@ public class RaceTurtle extends Turtle{
 	}
 	
 	/**
-	 * Moves the turtle 1-MAX_STEP_LENGTH steps forward.
+	 * Flyttar turtle 1-MAX_STEP_LENGTH(6) steg fram.
+	 * Skriv över denna för specialfall
 	 */
 	public void raceStep(){
 		forward(rand.nextInt(MAX_STEP_LENGTH)+1);
 	}
 	
+	/**
+	 * Skriv över denna för specialfall
+	 */
 	public String toString(){
 		return "Nummer " + number;
 	}
